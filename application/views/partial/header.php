f37b3ba6d56f2830d69c4c9520971a5f80b7be82
gokeel <ocky.harli@gmail.com>
2016-02-19 17:01:16 +0700

    V 2.8
    - rombak layout laporan grafis
    - tambah submenu left sidebar

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -19,10 +19,16 @@
     <!-- AdminLTE Skins. Choose a skin from the css/skins 
          folder instead of downloading all of them to reduce the load. -->
     <link href="themes/adminlte/dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+    <!-- daterange picker -->
+    <link href="themes/adminlte/plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+    <!-- Bootstrap Color Picker -->
+    <link href="themes/adminlte/plugins/colorpicker/bootstrap-colorpicker.min.css" rel="stylesheet"/>
+    <!-- Bootstrap time Picker -->
+    <link href="themes/adminlte/plugins/timepicker/bootstrap-timepicker.min.css" rel="stylesheet"/>
 
     <link rel="stylesheet" type="text/css" href="css/ospos.css"/>
 	<link rel="stylesheet" type="text/css" href="css/ospos_print.css" media="print" />
-	<?php if ($this->input->cookie('debug') == "true" || $this->input->get("debug") == "true") : ?>
+	<?php //if ($this->input->cookie('debug') == "true" || $this->input->get("debug") == "true") : ?>
 	<!-- start js template tags -->
 	<!--<script type="text/javascript" src="js/jquery-1.8.3.js" language="javascript"></script>-->
   <script src="<?php echo ADMIN_LTE_DIR;?>/plugins/jQuery/jQuery-2.1.3.min.js"></script>
@@ -47,11 +53,11 @@
 	<script type="text/javascript" src="js/tabcontent.js" language="javascript"></script>
 	<script type="text/javascript" src="js/thickbox.js" language="javascript"></script>
 	<!-- end js template tags -->
-    <?php else : ?>
+    <?php //else : ?>
     <!-- start minjs template tags -->
-    <script type="text/javascript" src="dist/opensourcepos.js?rel=cb9e5b15ec" language="javascript"></script>
+    <!--<script type="text/javascript" src="dist/opensourcepos.js?rel=cb9e5b15ec" language="javascript"></script>
     <!-- end minjs template tags -->       
-    <?php endif; ?>
+    <!--<?php //endif; ?>-->
 	<script type="text/javascript">
 		function logout(logout)
 		{
@@ -79,7 +85,7 @@
     <div class="wrapper">
       
       <header class="main-header">
-        <a href="<?php echo base_url(); ?>" class="logo"><b><?php echo $this->config->item('company') ?> </b></a>
+        <a href="<?php echo base_url();?>" class="logo"><b><?php echo $this->config->item('company') ?> </b></a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
           <!-- Sidebar toggle button-->
@@ -91,114 +97,12 @@
           </a>
           <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-              <!-- Messages: style can be found in dropdown.less-->
-              <li class="dropdown messages-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-envelope-o"></i>
-                  <span class="label label-success">4</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <li class="header">You have 4 messages</li>
-                  <li>
-                    <!-- inner menu: contains the actual data -->
-                    <ul class="menu">
-                      <li><!-- start message -->
-                        <a href="#">
-                          <div class="pull-left">
-                            <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
-                          </div>
-                          <h4>
-                            Support Team
-                            <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                          </h4>
-                          <p>Why not buy a new awesome theme?</p>
-                        </a>
-                      </li><!-- end message -->
-                    </ul>
-                  </li>
-                  <li class="footer"><a href="#">See All Messages</a></li>
-                </ul>
-              </li>
-              <!-- Notifications: style can be found in dropdown.less -->
-              <li class="dropdown notifications-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-bell-o"></i>
-                  <span class="label label-warning">10</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <li class="header">You have 10 notifications</li>
-                  <li>
-                    <!-- inner menu: contains the actual data -->
-                    <ul class="menu">
-                      <li>
-                        <a href="#">
-                          <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="footer"><a href="#">View all</a></li>
-                </ul>
-              </li>
-              <!-- Tasks: style can be found in dropdown.less -->
-              <li class="dropdown tasks-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-flag-o"></i>
-                  <span class="label label-danger">9</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <li class="header">You have 9 tasks</li>
-                  <li>
-                    <!-- inner menu: contains the actual data -->
-                    <ul class="menu">
-                      <li><!-- Task item -->
-                        <a href="#">
-                          <h3>
-                            Design some buttons
-                            <small class="pull-right">20%</small>
-                          </h3>
-                          <div class="progress xs">
-                            <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                              <span class="sr-only">20% Complete</span>
-                            </div>
-                          </div>
-                        </a>
-                      </li><!-- end task item -->
-                    </ul>
-                  </li>
-                  <li class="footer">
-                    <a href="#">View all tasks</a>
-                  </li>
-                </ul>
-              </li>
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                   <!-- <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image"/> -->
                   <span class="hidden-xs"><?php echo $this->lang->line('common_welcome')." $user_info->first_name $user_info->last_name"; ?></span>
                 </a>
-                <ul class="dropdown-menu">
-                  <li class="user-body">
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Followers</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Sales</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Friends</a>
-                    </div>
-                  </li>
-                  <!-- Menu Footer-->
-                  <li class="user-footer">
-                    <div class="pull-left">
-                      <a href="#" class="btn btn-default btn-flat">Profile</a>
-                    </div>
-                    <div class="pull-right">
-                      <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                    </div>
-                  </li>
-                </ul>
               </li>
             </ul>
           </div>
